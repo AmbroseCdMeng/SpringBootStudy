677069258e14746edbb709fbd0b2f72e9b927710
AmbroseCdMeng <mcd.951026@foxmail.com>
2020-06-20 14:32:08 +0800

    SpringBoot 环境搭建及基础配置

---
+++ chapter01-2/src/main/java/org/sang/config/TomcatConfig.java
@@ -0,0 +1,44 @@
+package org.sang.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.apache.catalina.Context;
+
+@Configuration
+public class TomcatConfig {
+    @Bean
+    TomcatServletWebServerFactory tomcatServletWebServerFactory(){
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory(){
+
+            @Override
+            protected  void postProcessContext(Context context){
+                SecurityConstraint constraint  = new SecurityConstraint();
+                constraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                constraint.addCollection(collection);
+                context.addConstraint(constraint);
+            }
+        };
+        factory.addAdditionalTomcatConnectors(createTomcatConnector());
+        return  factory;
+    }
+
+    /**
+     * 监听 http://localhost:8081/ 端口，重定向到 https://localhost:8080/
+     * @return
+     */
+    private Connector createTomcatConnector(){
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8081);
+        connector.setSecure(false);
+        connector.setRedirectPort(8080);
+        return connector;
+    }
+}
