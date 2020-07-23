4a3dcdabb4d1a1888751b2572336879cb3a94de2
AmbroseCdMeng <mcd.951026@foxmail.com>
2020-07-23 16:32:32 +0800

    SpringBoot 安全管理 —— Shiro

---
+++ chapter05-4/src/main/java/org/sang/controller/ExceptionController.java
@@ -0,0 +1,20 @@
+package org.sang.controller;
+
+import org.apache.shiro.authz.AuthorizationException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 全局异常处理器。本例主要处理授权异常
+ */
+
+@ControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(AuthorizationException.class)
+    public ModelAndView error(AuthorizationException e){
+        ModelAndView mv = new ModelAndView("unauthorized");
+        mv.addObject("error", e.getMessage());
+        return mv;
+    }
+}
